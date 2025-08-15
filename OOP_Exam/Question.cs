865c3d8767f8f62980c16fb5c6d88742ca5e1f24
mohamed-eldeghiedy <eldegedym@gmail.com>
2025-08-15 22:14:46 +0300

    exam

---
+++ OOP_Exam/Question.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace OOP_Exam
+{
+    public abstract class Question
+    {
+
+        public string Header { get; set; }
+        public string Body { get; set; }
+        public double Mark { get; set; }
+
+        public Answer[] AnswerList { get; set; }
+        public Answer RightAnswer { get; set; }
+
+        public Question(string header, string body, double mark)
+        {
+            Header = header;
+            Body = body;
+            Mark = mark;
+
+        }
+
+        public abstract void ShowQuestion();
+
+        public object Clone()
+        {
+            Question clone = (Question)this.MemberwiseClone();
+            clone.AnswerList = (Answer[])this.AnswerList.Clone();
+            clone.RightAnswer = (Answer)this.RightAnswer.Clone();
+            return clone;
+        }
+
+        public int CompareTo(Question other)
+        {
+            return Header.CompareTo(other.Header);
+        }
+
+        public override string ToString()
+        {
+            return $"Header: {Header}, Body: {Body}, Mark: {Mark}, RightAnswer: {RightAnswer}";
+        }
+    }
+
+
+    public class MCQQuestion : Question
+    {
+        public MCQQuestion(string body, double mark, Answer[] answers, Answer reightAnswer) : base("Multiple Choice Question", body, mark)
+        {
+            AnswerList = answers;
+            RightAnswer = reightAnswer;
+        }
+        public override void ShowQuestion()
+        {
+            Console.WriteLine($"Header: {Header}");
+            Console.WriteLine($"Body: {Body}");
+            Console.WriteLine($"Mark: {Mark}");
+            Console.WriteLine("Answers:");
+            foreach (var Ans in AnswerList)
+            {
+                Console.WriteLine(Ans);
+            }
+            Console.WriteLine($"Right Answer: {RightAnswer}");
+        }
+    }
+
+
+    public class TFQuestion : Question
+    { 
+        public TFQuestion(string body , double mark , Answer rightAnser ) : base("True/False Question", body, mark)
+        {
+            AnswerList = new Answer[]
+            {
+                new Answer(1, "True"),
+                new Answer(2, "False")
+            };
+            RightAnswer = rightAnser;
+        }
+
+        public override void ShowQuestion()
+        {
+            Console.WriteLine($"Header: {Header}");
+            Console.WriteLine($"Body: {Body}");
+            Console.WriteLine($"Mark: {Mark}");
+            Console.WriteLine("Answers:");
+            foreach (var Ans in AnswerList)
+            {
+                Console.WriteLine(Ans);
+            }
+            Console.WriteLine($"Right Answer: {RightAnswer}");
+        }
+
+    }
+}
---
+++ OOP_Exam/Subject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace OOP_Exam
+{
+    public class Subject 
+    {
+
+        public int SubjectId { get; set; }
+        public string SubjectName { get; set; }
+        public Exam SubjectExam { get; set; }
+
+
+        public Subject(int subjectId, string subjectName)
+        {
+            SubjectId = subjectId;
+            SubjectName = subjectName;
+           
+        }
+
+        public void CreateExam(Exam exam)
+        {
+            SubjectExam = exam;
+        }
+
+        public override string ToString()
+        {
+            return $"Subject: {SubjectName} (ID: {SubjectId})";
+        }
+    }
+}
